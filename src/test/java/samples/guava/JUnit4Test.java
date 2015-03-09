b37d82c185bc372918c1d10f8996ecae23369b6b
Juan Martín Sotuyo Dodero <jmsotuyo@monits.com>
2015-03-09 12:39:23 -0300

    Improve `@VisibleForTesting` detector
    Summary:
     - Skip whole classes if they are JUnit 3 or 4 tests.
        - This avoids false positive in other JUnit 4 methods, which were not `@Test`
        - Also speeds up checks a bit by avoiding going into the opcodes of junit classes
    
    Test Plan: mvn test
    
    Reviewers: pabad
    
    Reviewed By: pabad
    
    Differential Revision: http://ph.monits.com/D10148

--- src/test/java/samples/guava/JUnit4Test.java
+++ src/test/java/samples/guava/JUnit4Test.java
@@ -3,18 +3,36 @@
  */
 package samples.guava;
 
-import jp.co.worksap.oss.findbugs.guava.UnexpectedAccessDetector;
-
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
 /**
- * Class used only for {@link UnexpectedAccessDetector}-tests
+ * Class used only for
+ * {@link jp.co.worksap.oss.findbugs.guava.UnexpectedAccessDetector}-tests
+ * 
  * @author tolina GmbH
  *
  */
 @Ignore("Not a real test, but used as test sample")
 public class JUnit4Test {
+
+	@Before
+	void setUp() {
+		new MethodWithVisibleForTesting().method();
+	}
+
+	@BeforeClass
+	void onlyOnce() {
+		new MethodWithVisibleForTesting().method();
+	}
+
+	@After
+	void tearDown() {
+		new MethodWithVisibleForTesting().method();
+	}
 
 	@Test
 	void test() {
